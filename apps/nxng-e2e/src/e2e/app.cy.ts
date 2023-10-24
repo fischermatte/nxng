8e3de0eb6e1a63c8eb0ecc5b4a6e5bd45335a985
fischermatte <fischermatte@posteo.net>
2023-10-24 22:32:38 +0200

    chore: fixed e2e

--- apps/nxng-e2e/src/e2e/app.cy.ts
+++ apps/nxng-e2e/src/e2e/app.cy.ts
@@ -4,10 +4,7 @@ describe('nxng', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome nxng');
+       // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome NgNx');
   });
 });
